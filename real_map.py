90d9bba4eded6b6d87a3240461f12edb6d7405ef
karturbis <vulkanera42@gmx.ch>
2024-11-01 06:24:15 +0100

    add calculating the distance

--- real_map.py
+++ real_map.py
@@ -1,9 +1,10 @@
+import json
+from geopy.distance import distance
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
-import json
 
-map_file = "testmap.geojson"
+map_file = "test_map_data/test_01.geojson"
 
 with open(map_file) as map_data_raw:
     map_data = json.load(map_data_raw)
@@ -15,7 +16,7 @@ map_data_edges = {}
 
 for element in map_data["elements"]:
     if element["type"] == "node":
-        map_data_nodes[element["id"]] = (element["lat"], element["lon"])
+        map_data_nodes[element["id"]] = (element["lon"], element["lat"])
     elif element["type"] == "way":
         map_data_edges[element["id"]] = element["nodes"]
 
@@ -27,7 +28,20 @@ for node, position in map_data_nodes.items():
     nodes.append(node)
     pos[node] = np.array([position[0], position[1]])
 
-G.add_nodes_from(nodes)
 
-nx.draw(G, pos, node_size = 1)
+for key, edge in map_data_edges.items():
+    for i, start_node in enumerate(edge):
+        try:
+            G.add_edge(
+                start_node,
+                edge[i + 1],
+                length=distance(
+                    map_data_nodes[start_node], map_data_nodes[edge[i + 1]]
+                ),
+            )
+        except IndexError:
+            continue
+
+G.add_nodes_from(nodes)
+nx.draw_networkx_edges(G, pos)
 plt.show()
