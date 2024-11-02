0c19c8781520bad70e22c06b4e806421145eb8aa
karturbis <vulkanera42@gmx.ch>
2024-11-03 00:49:12 +0100

    import dijkstra

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from real_map import generate_map_graph
+from dijkstra import dijkstra
 
 G, pos = generate_map_graph()
 nx.draw(G, pos, node_size = 1)
