35b954abace1e776cda9092e266690cd49843bca
karturbis <vulkanera42@gmx.ch>
2024-11-03 16:35:12 +0100

    make parameters easier to change

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,7 @@
 import time
 import random
 import networkx as nx
+import matplotlib.pyplot as plt
 from real_map import generate_map_graph
 from a_star import a_star
 from a_star import geopy_dis
@@ -8,58 +9,75 @@ from a_star import flatearther_dis
 from dijkstra import dijkstra
 
 SEED = random.randint(1, 22222222222222222222222222222222222)
-#SEEED = 1931315997966322171689304248493832
+# SEEED = 1931315997966322171689304248493832
 ITERATIONS = 100
-print(f"Seed = {SEED}")  # printing the seed at the begin, so if the program crashes, it is printed
+MIN_EDGE_LENGTH = 1
+MAX_EDGE_LENGTH = 100
+GRAPH_SIZE = 1000
+REAL_MAP = False
+DRAW_GRAPH = True
+DRAW_EDGE_LABELS = False
+print(
+    f"Seed = {SEED}"
+)  # printing the seed at the begin, so if the program crashes, it is printed
 random.seed(SEED)
 
-G = nx.binomial_graph(1000, 56)
-pos = nx.spring_layout(G)
-
-weights = [random.randint(0, 100 + i) for i in range(len(G.edges()))]
-
-nx.set_edge_attributes(G, values = 6, name = 'weight')
-
+if REAL_MAP:
+    G, pos = generate_map_graph()  # create a Graph from a real map
+else:
+    # create a random binomial Graph:
+    G = nx.binomial_graph(GRAPH_SIZE, 10 / GRAPH_SIZE)
+    pos = nx.spring_layout(G)
+    weights = nx.edge_betweenness_centrality(G, normalized=False)
+    for edge in G.edges():
+        weights[edge] = random.randint(MIN_EDGE_LENGTH, MAX_EDGE_LENGTH)
+    nx.set_edge_attributes(G, values=weights, name="weight")
 nodes = list(G.nodes())
 
+# init the timer sums:
 tda = 0
 taa = 0
 tga = 0
 tna = 0
 
 for i in range(ITERATIONS):
-
-    node_1 = nodes[random.randint(1, len(nodes)-1)]
-    #node_1 = 7660414077
+    node_1 = nodes[random.randint(1, len(nodes) - 1)]
+    # node_1 = 7660414077
     print(node_1)
-    node_2 = nodes[random.randint(1, len(nodes)-1)]
-    #node_2 = 271900861
+    node_2 = nodes[random.randint(1, len(nodes) - 1)]
+    # node_2 = 271900861
     print(node_2)
     td = time.time()
-
     path_djk = dijkstra(G, pos, node_1, node_2)
     tda += time.time() - td
     ta = time.time()
-    try:
-        path_ast = a_star(G, pos, node_1, node_2, flatearther_dis)
-    except Exception:
-        print(f"Error {e.message} has occured")
+    path_ast = a_star(G, pos, node_1, node_2, flatearther_dis)
     taa += time.time() - ta
-    """tg = time.time()
-    try:
+    if REAL_MAP:
+        taa += time.time() - ta
+        tg = time.time()
         path_gas = a_star(G, pos, node_1, node_2, geopy_dis)
-    except Exception:
-        print(f"Error {e} has occured")
-    tga += time.time() - tg"""
+        tga += time.time() - tg
     tn = time.time()
-
-    path_nx = nx.shortest_path(G, node_1, node_2, weight="weight")
-
+    try:
+        path_nx = nx.shortest_path(G, node_1, node_2, weight="weight")
+    except nx.NetworkXNoPath:
+        print(f"No path from {node_1} to {node_2}")
     tna += time.time() - tn
 
 # printing the average time each algorithm needs
 print(f"Average time dj: {tda/ITERATIONS}")
 print(f"Average time a*: {taa/ITERATIONS}")
-print(f"Average time ga: {tga/ITERATIONS}")
+if REAL_MAP:
+    print(f"Average time ga: {tga/ITERATIONS}")
 print(f"Average time nx: {tna/ITERATIONS}")
-print(f"The seed is: {SEED}")  # printing the Seed at the end of the program, so it can easily be obtained
+print(
+    f"The seed is: {SEED}"
+)  # printing the Seed at the end of the program, so it can easily be obtained
+if DRAW_GRAPH:
+    nx.draw(G, pos)
+    if DRAW_EDGE_LABELS:
+        nx.draw_networkx_edge_labels(
+            G, pos, edge_labels={(u, v): d["weight"] for u, v, d in G.edges(data=True)}
+        )
+    plt.show()
