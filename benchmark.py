08fb3d18ce3808edcec8eda87d28b44cc996a8d7
karturbis <vulkanera42@gmx.ch>
2024-11-03 05:00:09 +0100

    improve the benchmark, by choosing Nodes randomly and repeat the beanchmark a given amount of iterations, in the end print the Average times for the algorithms

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,5 @@
 import time
+import random
 import networkx as nx
 from real_map import generate_map_graph
 from a_star import a_star
@@ -6,22 +7,47 @@ from a_star import geopy_dis
 from a_star import flatearther_dis
 from dijkstra import dijkstra
 
-G, pos = generate_map_graph()
-node_1 = 1770439267
-node_2 = 11201847066
+SEED = random.randint(1, 22222222222222222222222222222222222)
+#SEEED = 1931315997966322171689304248493832
+ITERATIONS = 1000
+print(f"Seed = {SEED}")  # printing the seed at the begin, so if the program crashes, it is printed
+random.seed(SEED)
 
-t = time.time()
-path_djk = dijkstra(G, pos, node_1, node_2)
-print(f"Dijkstra finished in {time.time() - t}")
-t = time.time()
-path_ast = a_star(G, pos, node_1, node_2, flatearther_dis)
-print(f"A* finished in: {time.time() - t}")
-t = time.time()
-path_nx = nx.shortest_path(G, node_1, node_2)
-print(f"Networkx finished in {time.time() -t}")
-print("-----------------------------------")
-print(path_djk)
-print(path_ast)
-print(path_nx)
-print("-----------------------------------")
-print(nx.shortest_path_length(G, node_1, node_2))
+G, pos = generate_map_graph()
+
+nodes = list(G.nodes())
+
+tda = 0
+taa = 0
+tna = 0
+
+for i in range(ITERATIONS):
+
+    node_1 = nodes[random.randint(1, len(nodes)-1)]
+    print(node_1)
+    node_2 = nodes[random.randint(1, len(nodes)-1)]
+    print(node_2)
+    td = time.time()
+    try:
+        path_djk = dijkstra(G, pos, node_1, node_2)
+    except Exception:
+        print("An Error has occured")
+    tda += time.time() - td
+    ta = time.time()
+    try:
+        path_ast = a_star(G, pos, node_1, node_2, flatearther_dis)
+    except Exception:
+        print("An Error has occured")
+    taa += time.time() - ta
+    tn = time.time()
+    try:
+        path_nx = nx.shortest_path(G, node_1, node_2, weight="weight")
+    except Exception:
+        print("An Error has occured")
+    tna += time.time() - tn
+
+# printing the average time each algorithm needs
+print(f"Average time dj: {tda/ITERATIONS}")
+print(f"Average time a*: {taa/ITERATIONS}")
+print(f"Average time nx: {tna/ITERATIONS}")
+print(f"The seed is: {SEED}")  # printing the Seed at the end of the program, so it can easily be obtained
