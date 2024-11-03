6138508a589a3e9f60b09567b9011a35ccd4fa7f
karturbis <vulkanera42@gmx.ch>
2024-11-03 16:35:22 +0100

    add comment

--- dijkstra.py
+++ dijkstra.py
@@ -1,6 +1,8 @@
+"""Contains the dijkstra function, which
+uses Dijkstras Algorithm to find shortest path."""
 from a_star import a_star
 
-def dijkstra(G, pos, start_node, end_node, arg1=None):
+def dijkstra(G, pos, start_node, end_node, _=None):
     """Finds the shortest path between two nodes,
     using Dijkstra's algorithm, which is equivalent
     to the A* algorithm with no heuristic"""
