7907e281915f4e4054694934b121cef071145875
karturbis <vulkanera42@gmx.ch>
2024-11-03 05:22:46 +0100

    add a* with geopy distance calculation to the benchmark

--- benchmark.py
+++ benchmark.py
@@ -9,7 +9,7 @@ from dijkstra import dijkstra
 
 SEED = random.randint(1, 22222222222222222222222222222222222)
 #SEEED = 1931315997966322171689304248493832
-ITERATIONS = 1000
+ITERATIONS = 100
 print(f"Seed = {SEED}")  # printing the seed at the begin, so if the program crashes, it is printed
 random.seed(SEED)
 
@@ -19,13 +19,16 @@ nodes = list(G.nodes())
 
 tda = 0
 taa = 0
+tga = 0
 tna = 0
 
 for i in range(ITERATIONS):
 
     node_1 = nodes[random.randint(1, len(nodes)-1)]
+    #node_1 = 7660414077
     print(node_1)
     node_2 = nodes[random.randint(1, len(nodes)-1)]
+    #node_2 = 271900861
     print(node_2)
     td = time.time()
     try:
@@ -39,6 +42,12 @@ for i in range(ITERATIONS):
     except Exception:
         print("An Error has occured")
     taa += time.time() - ta
+    tg = time.time()
+    try:
+        path_gas = a_star(G, pos, node_1, node_2, geopy_dis)
+    except Exception:
+        print("An Error has occured")
+    tga += time.time() - tg
     tn = time.time()
     try:
         path_nx = nx.shortest_path(G, node_1, node_2, weight="weight")
@@ -49,5 +58,6 @@ for i in range(ITERATIONS):
 # printing the average time each algorithm needs
 print(f"Average time dj: {tda/ITERATIONS}")
 print(f"Average time a*: {taa/ITERATIONS}")
+print(f"Average time ga: {tga/ITERATIONS}")
 print(f"Average time nx: {tna/ITERATIONS}")
 print(f"The seed is: {SEED}")  # printing the Seed at the end of the program, so it can easily be obtained
