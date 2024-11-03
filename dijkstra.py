da8c8d967276af1eda8dc9a9e1ba0aa92ab41013
karturbis <vulkanera42@gmx.ch>
2024-11-03 05:00:18 +0100

    add comment

--- dijkstra.py
+++ dijkstra.py
@@ -1,4 +1,7 @@
 from a_star import a_star
 
-def dijkstra(G, start_node, end_node, arg1=None, arg2= None):
-    return a_star(G, start_node, end_node, None, None)
+def dijkstra(G, pos, start_node, end_node, arg1=None):
+    """Finds the shortest path between two nodes,
+    using Dijkstra's algorithm, which is equivalent
+    to the A* algorithm with no heuristic"""
+    return a_star(G, pos, start_node, end_node, None)
