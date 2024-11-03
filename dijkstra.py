a5b6c3c8342eeaeead8ec7fbecd4c643c2d294f9
karturbis <vulkanera42@gmx.ch>
2024-11-03 03:08:51 +0100

    take to affitional positional args, so there do not have to be changes in main.py, depending on the algorithm

--- dijkstra.py
+++ dijkstra.py
@@ -1,4 +1,4 @@
 from a_star import a_star
 
-def dijkstra(G, start_node, end_node):
+def dijkstra(G, start_node, end_node, arg1=None, arg2= None):
     return a_star(G, start_node, end_node, None, None)
