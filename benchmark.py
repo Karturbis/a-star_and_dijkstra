ae8c352eff32bdbf326322a226bfc0e2fcb041c7
karturbis <vulkanera42@gmx.ch>
2024-11-05 17:17:31 +0100

    add comments

--- benchmark.py
+++ benchmark.py
@@ -1,26 +1,34 @@
-import time
-import random
-import networkx as nx
-import matplotlib.pyplot as plt
-from real_map import generate_map_graph
+"""Program to benchmark the Dijkstra and A*
+Algorithm in different situations, using
+nx.shortest_path() as a comparison"""
+import time  # to benchmark time te algorithms need
+import random  # to make automated benchmarks with random situatuins
+import networkx as nx  # to implement the graph
+import matplotlib.pyplot as plt  # to show the graph in case this is needed
+from real_map import generate_map_graph  # to generate a graph of a real map
+# to use A* algorithm with differen heuristics
 from a_star import a_star
 from a_star import geopy_dis
 from a_star import flatearther_dis
+# to use Dijkstra algorithm
 from dijkstra import dijkstra
 
+# make random Seed, so benchmarks are different, but can be recreated
 SEED = random.randint(1, 22222222222222222222222222222222222)
 # SEEED = 1931315997966322171689304248493832
+
+# Parameters for the benchmark:
 ITERATIONS = 100
-MIN_EDGE_LENGTH = 1
-MAX_EDGE_LENGTH = 100
-GRAPH_SIZE = 1000
-REAL_MAP = True
+MIN_EDGE_LENGTH = 1  # only relevant, if not real_map
+MAX_EDGE_LENGTH = 100  # see above
+GRAPH_SIZE = 1000  # see above
+REAL_MAP = True  # determines, wether a real map, or a random graph is used
 DRAW_GRAPH = True
-DRAW_EDGE_LABELS = False
+DRAW_EDGE_LABELS = False  # WARNING, uses a lot of resources in big graphs
 print(
     f"Seed = {SEED}"
 )  # printing the seed at the begin, so if the program crashes, it is printed
-random.seed(SEED)
+random.seed(SEED)  # setting the random seed
 
 if REAL_MAP:
     G, pos = generate_map_graph()  # create a Graph from a real map
@@ -47,17 +55,20 @@ for i in range(ITERATIONS):
     #node_2 = nodes[random.randint(1, len(nodes) - 1)]
     node_2 = 3428399062
     print(node_2)
+    # messure time Dijkstra needs to complete the path search:
     td = time.time()
     path_djk = dijkstra(G, pos, node_1, node_2)
-    tda += time.time() - td
+    tda += time.time() - td  # adding Dijkstras time to Dijkstras overall time
+    # messure time A* with flatearther distance needs to complete the path search:
     ta = time.time()
     path_ast = a_star(G, pos, node_1, node_2, flatearther_dis)
     taa += time.time() - ta
     if REAL_MAP:
-        taa += time.time() - ta
+        # messure the time for A* with geopys distance function as heuristic:
         tg = time.time()
         path_gas = a_star(G, pos, node_1, node_2, geopy_dis)
         tga += time.time() - tg
+    # messure the time nx needs, as a reference time
     tn = time.time()
     try:
         path_nx = nx.shortest_path(G, node_1, node_2, weight="weight")
@@ -75,9 +86,9 @@ print(
     f"The seed is: {SEED}"
 )  # printing the Seed at the end of the program, so it can easily be obtained
 if DRAW_GRAPH:
-    nx.draw(G, pos)
+    nx.draw(G, pos)  # make the graph drawable
     if DRAW_EDGE_LABELS:
         nx.draw_networkx_edge_labels(
             G, pos, edge_labels={(u, v): d["weight"] for u, v, d in G.edges(data=True)}
         )
-    plt.show()
+    plt.show()  # open a window and use matplotlib to draw the graph
